e0a8cd1ceb8bc748a97bf891c9690549c8be6a25
Kyle Sylvestre <kyles3133@gmail.com>
2026-09-02 21:06:21 -0400

    guard against null mControllerWidget

--- source/CircleShoot/CircleShootApp.cpp
+++ source/CircleShoot/CircleShootApp.cpp
@@ -1920,7 +1920,7 @@ void CircleShootApp::HandleEvent(SDL_Event *ev)
                     mBoard->mTransitionMgr->FinishAllTextBlurbs();
                     mBoard->mTransitionMgr->mResetFrame = mBoard->mTransitionMgr->mStateCount;
                 }
-                else
+                else if (mControllerWidget)
                 {
                     int x = mControllerWidget->mX + mControllerWidget->mWidth / 2.0f;
                     int y = mControllerWidget->mY + mControllerWidget->mHeight / 2.0f;
